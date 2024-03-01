d016c1452b856b4d0b24ab904d9c67476395433e
XiangFei89213 <143454666+XiangFei89213@users.noreply.github.com>
2024-03-01 09:45:43 -0800

    Update main.rs

--- find_prime/src/main.rs
+++ find_prime/src/main.rs
@@ -1,13 +1,39 @@
 fn sieve(n:usize){
     
     println!("hello from function {}" , n);
-    let prime: Vec<bool> = vec![true; n];
+    let mut prime: Vec<bool> = vec![true; n];
     
-    println!("{:?}", prime);
+    //mutable arrray
+    // println!("{:?}", prime);
      
-    prime[2] =false;
-    //println!("{:?}", prime);
+    // prime[2] =false;
+    // println!("{:?}", prime);
+    
+    // for loop
+    let mut i:usize =2;
+    let mut p:i32 ;
+    let mut done = false;
+    
+    while !done{
+        let i = i+1;
+        
+        if prime[i] == true{
+            for p in (i*i) .. n {
+                prime[i] = false;
+            }
+        }
+        
+        if i*i <= n {
+            done = true;
+        }
+    }
 
+    // print result
+    for p in 2..n+1{
+        if prime[p] == true{
+            println!("{}" , p)
+        }
+    }
     
 }
 fn main() {
