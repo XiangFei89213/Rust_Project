43cb41702a328abf13d9544712da40a23e42d876
Sophia Yu <yusophiayu@gmail.com>
2024-04-13 17:20:40 -0700

    change to read from a array and print frime number in the array

--- Find_prime/Trial_division.rs
+++ Find_prime/Trial_division.rs
@@ -23,15 +23,40 @@ fn trial_division(n: usize) -> bool {
 }
 
 // Driver code
-fn main() {
-    let mut num_of_prime = 0;
-    let prime = 1000;
+// fn main() {
+//     // let mut num_of_prime = 0;
+//     // let prime = 1000;
+//     let numbers = [2, 3, 5, 6, 7, 11, 13, 17, 19, 23];
     
-    for n in 1..prime {
-        if trial_division(n) {
-            num_of_prime+=1;
-            //print!("{} ", n);
+//     // for n in 1..prime {
+//     //     if trial_division(n) {
+//     //         num_of_prime+=1;
+//     //         //print!("{} ", n);
+//     //     }
+//     // }
+
+//     println!("Number of prime numbers: ");
+//     for &num in numbers.iter() {
+//         if trial_division(num) {
+//             print!("{}", num)
+//         }
+//     }
+    
+//     // println!("number of prime : {}", num_of_prime);
+// }
+
+fn main() {
+    let numbers = [2, 3, 5, 6, 7, 11, 13, 17, 19, 23];
+
+    println!("Prime numbers in the array:");
+    for &num in numbers.iter() {
+        if trial_division(num) {
+            print!("{} ", num);
         }
+        else if num == 2 {
+            print!("{} ", &num);
+        }
+            
     }
-    println!("number of prime : {}", num_of_prime);
+    println!(); // Print a newline after printing all prime numbers
 }
