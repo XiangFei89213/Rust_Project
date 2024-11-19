63e159d6935dbc59a6266bd7f4c90921d85a3a0d
Sophia Yu <yusophiayu@gmail.com>
2024-11-19 00:12:46 -0800

    new file u64

---
+++ u64/src/main.rs
@@ -0,0 +1,5 @@
+use std::fmt::Display;
+fn main() {
+    let prime = assert_eq!(u8::min_value(),0);
+    println!("hello world on linux: {}", prime);
+}
---
+++ u64/target/debug/deps/u64-99cd916028bea676.d
@@ -0,0 +1,5 @@
+/home/user01/git_file/Rust_Project/u64/target/debug/deps/u64-99cd916028bea676: src/main.rs
+
+/home/user01/git_file/Rust_Project/u64/target/debug/deps/u64-99cd916028bea676.d: src/main.rs
+
+src/main.rs:
